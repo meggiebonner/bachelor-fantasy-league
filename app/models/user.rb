87da5a51cbabe6687dc2700b6be2f25ba5432a00
Meggie Bonner <meggie.bonner@opower.com>
2015-12-31 20:48:30 -0500

    remove regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,11 +13,8 @@ class User < ActiveRecord::Base
 
   def self.authenticate(username_or_email="", login_password="")
 
-    if  EMAIL_REGEX.match(username_or_email)    
-      user = User.find_by_email(username_or_email)
-    else
-      user = User.find_by_username(username_or_email)
-    end
+
+    user = User.find_by_username(username_or_email)
 
     if user && user.match_password(login_password)
       return user
