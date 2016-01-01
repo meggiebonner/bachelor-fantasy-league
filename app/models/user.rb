58636a6cbc26d6bef296a167b17f44df674f7b0d
Meggie Bonner <meggie.bonner@opower.com>
2015-12-31 20:56:03 -0500

    update email regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,23 @@ class User < ActiveRecord::Base
   before_save :encrypt_password
   after_save :clear_password
 
+  EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\Z/i
   validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
-  validates :email, :presence => true, :uniqueness => true
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   validates :password, :confirmation => true
   #Only on Create so other actions like update password attribute can be nil
   validates_length_of :password, :in => 6..20, :on => :create
 
+  attr_accessible :username, :email, :password, :password_confirmation
+
+
   def self.authenticate(username_or_email="", login_password="")
 
-
-    user = User.find_by_username(username_or_email)
+    if  EMAIL_REGEX.match(username_or_email)    
+      user = User.find_by_email(username_or_email)
+    else
+      user = User.find_by_username(username_or_email)
+    end
 
     if user && user.match_password(login_password)
       return user
