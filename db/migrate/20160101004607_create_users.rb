3587bbff237f06fb75ec8e8992b6ced0810cf75f
Meggie Bonner <meggie.bonner@opower.com>
2016-01-01 22:59:53 -0800

    fix user bug

--- db/migrate/20160101004607_create_users.rb
+++ db/migrate/20160101004607_create_users.rb
@@ -5,6 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :email
       t.string :encrypted_password 
       t.string :salt
+      t.string :paid
       t.timestamps
     end
   end
